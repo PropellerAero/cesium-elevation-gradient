27a3162f4826990e10ba694f87773ccbb635cb86
Chris Cooper <chris@propelleraero.com.au>
2016-07-20 09:02:23 +1000

    avoid recreating program

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function setUpElevationGradient(viewer) {
 
 function initCameraLocation(viewer) {
     var target = Cartesian3.fromDegrees(130.7359, -25.2990);
-    var offset = new Cartesian3(1700, 1700, 1700);
+    var offset = new Cartesian3(1500, 1500, 3000);
     viewer.camera.lookAt(target, offset);
     viewer.camera.lookAtTransform(Matrix4.IDENTITY);
 }
